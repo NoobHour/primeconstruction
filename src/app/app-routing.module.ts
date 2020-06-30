aac809462118782c322ad659f8833fdc430045ae
NoobHour <corbinneubauer@gmail.com>
2020-06-30 13:42:28 -0500

    3 files modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
+
+
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { AuthGuard } from './_helpers';
+
+const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
+    { path: 'account', loadChildren: accountModule },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+/* import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorRoutingModule } from './error-routing/error-routing.module';
 import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { ComponentsComponent } from './components/components.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AuthGuard } from './_helpers';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
@@ -20,3 +48,4 @@ export const routes: Routes = [
 })
 export class AppRoutingModule {
 }
+*/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,25 @@
 import { Component } from '@angular/core';
+
+import { AccountService } from './_services';
+import { User } from './_models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    user: User;
+
+    constructor(private accountService: AccountService) {
+        this.accountService.user.subscribe(x => this.user = x);
+    }
+
+    logout() {
+        this.accountService.logout();
+    }
+}
+
+
+
+
+/* import { Component } from '@angular/core';
 import { routes } from './app-routing.module';
 
 @Component({
@@ -10,3 +31,4 @@ export class AppComponent {
   title = 'Prime Construction';
   routes = routes.filter(r => r.data);
 }
+*/
