6a4048603fcab34c41ae43bfd14243de60149cd1
NoobHour <corbinneubauer@gmail.com>
2020-06-30 13:48:31 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {
   IgxGridModule, IgxBottomNavModule, IgxAvatarModule, IgxIconModule, IgxRippleModule,
   IgxListModule, IgxInputGroupModule, IgxFilterModule
@@ -13,13 +14,17 @@ import { ComponentsComponent } from './components/components.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { fakeBackendProvider } from './_helpers';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers'
+import { AlertComponent } from './_components';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
     HomeComponent,
-    ContactsComponent
+    ContactsComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
@@ -34,10 +39,18 @@ import { ContactsComponent } from './contacts/contacts.component';
     IgxListModule,
     IgxInputGroupModule,
     IgxFilterModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-}
+};
